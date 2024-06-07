1a91c8499cd056b28f76cca17493d10a453568d1
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-06-08 00:51:47 +0900

    プロジェクトをセット

---
+++ src/main/java/raisetech/student/management/services/StudentService.java
@@ -0,0 +1,26 @@
+package raisetech.student.management.services;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentCourse;
+import raisetech.student.management.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+  private final StudentRepository repository;
+
+  public StudentService(StudentRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Student> searchStudentList() {
+    return repository.search();
+  }
+
+  public List<StudentCourse> searchStudentsCourseList() {
+    return repository.searchStudentsCourses();
+  }
+
+}
